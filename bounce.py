b6b77db00a8cee4c496a06cbb9b2b2880fcb1cc8
Bobby <bobbytradingtime@gmail.com>
2021-10-24 20:15:51 -0400

    Fix bugs

--- bounce.py
+++ bounce.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from ibapi.contract import Option
+from ibapi.contract import *
 
 class Action(Enum):
     Buy = 1
@@ -9,7 +9,7 @@ class SupportType(Enum):
     Low = 1
     High = 2
 
-class Option:
+class BounceOption:
     def __init__(self, equity_symbol, put_call, expiration_date, strike_price):
         contract = Contract()
         contract.symbol = equity_symbol
@@ -20,7 +20,15 @@ class Option:
         contract.right = put_call
         contract.strike = strike_price
         contract.multiplier = "100"
-        return contract
+        self.contract = contract
+        self.__repr__ = self.__str__
+
+    def __str__(self):
+        return "[%s %s %s %s]" % (
+                self.contract.symbol,
+                self.contract.lastTradeDateOrContractMonth,
+                self.contract.right,
+                self.contract.strike)
 
 class BounceTrade:
     def __init__(
@@ -32,10 +40,11 @@ class BounceTrade:
             ticks
             ):
         self.signal_symbol = signal_symbol
-        self.treshold_price = threshold_price
+        self.threshold_price = threshold_price
         self.support_type = support_type
         self.trade_symbol = trade_symbol
         self.ticks = ticks
+        self.__repr__ = self.__str__
 
     def onUpdate():
         pass
@@ -44,18 +53,18 @@ class BounceTrade:
         pass
 
     def __str__(self):
-        print(self.signal_symbol,
-                self.threshold_price,
-                self.support_type,
-                self.trade_symbol,
-                self.ticks = ticks)
+        return  (str(self.signal_symbol) + " "
+                + str(self.threshold_price) + " "
+                + str(self.support_type) + " "
+                + str(self.trade_symbol) + " "
+                + str(self.ticks))
 
 bounces = []
 
-bounces.append(BounceTrade("DKNG", 46.00, SupportType.Low, Option("DKNG", "C", "20211029", 48.00), 0.10))
+bounces.append(BounceTrade("DKNG", 46.00, SupportType.Low, BounceOption("DKNG", "C", "20211029", 48.00), 0.10))
 
-for b in bounce:
-    print(str(bounces))
+for b in bounces:
+    print(b)
 
 ###### TODO ######
 # 3. Subscribe to market data
